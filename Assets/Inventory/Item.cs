82c08cbea22376dafbb194538090027e6e2c6b5e
Numenization <nicholas.spencer5@gmail.com>
2018-02-28 21:48:30 -0500

    Started working on inventory systems

---
+++ Assets/Inventory/Item.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour {
+
+	public string itemName;
+	public string description;
+	public double baseValue;
+	public double pickupRadius;
+	public Sprite sprite;
+	public GameObject itemCard;
+
+	public void InRadius() {
+		// this is called when the player walks near the item
+	}
+
+	public void LeaveRadius() {
+		// this is called when the player walks away from the item
+	}
+
+	public void PickUp(Inventory inventory) {
+		// this is called when the player uses the interact key near the item
+	}
+
+	public void Move(Transform transform, Vector2 offset) {
+
+	}
+}
---
+++ Assets/Inventory/ItemSO.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Default Item Name", menuName = "Item", order = 1)]
+public class ItemSO : ScriptableObject {
+
+	public string itemName;
+	public GameObject gameObject;
+}
